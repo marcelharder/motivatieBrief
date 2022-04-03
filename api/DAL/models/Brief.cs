ab5e061a71842c8763a93fdeb3c2aeb1c3be22b6
marcel <m_harder88@yahoo.com>
2022-04-03 16:38:52 +0200

    starting with db

---
+++ api/DAL/models/Brief.cs
@@ -0,0 +1,26 @@
+using System;
+namespace api.DAL.data
+{
+    public class Brief
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public User User { get; set; }
+        public string line_1 { get; set; }
+        public string line_2 { get; set; }
+        public string line_3 { get; set; }
+        public string line_4 { get; set; }
+        public string line_5 { get; set; }
+        public string line_6 { get; set; }
+        public string line_7 { get; set; }
+        public string line_8 { get; set; }
+        public string line_9 { get; set; }
+        public string line_10 { get; set; }
+        public string line_11 { get; set; }
+        public string line_12 { get; set; }
+        public string line_13 { get; set; }
+        public string line_14 { get; set; }
+        public string line_15 { get; set; }
+   
+    }
+}
